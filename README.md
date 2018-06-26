b013f7c5544ff4d0fcd4a1f67347d7259e79558d
Marc3842h <git@marcsteiner.me>
2018-06-26 13:16:57 +0200

    Add support for continuous integration on CircleCI

--- README.md
+++ README.md
@@ -1,38 +1,48 @@
-# RPC Wine
+# RPC Wine [![CI](https://img.shields.io/circleci/project/github/Marc3842h/rpc-wine.svg)](https://circleci.com/gh/Marc3842h/rpc-wine)
 
-Making Discord RPC actually _rich_ by adding support for Wine.
-This is achieved by creating a Wine DLL which overrides the original
-`discord-rpc.dll` file.
-
-## Dependencies
-
-* [RapidJson](http://rapidjson.org/)
+`discord-rpc.dll` implementation for Wine allowing your Wine games to interact
+with your native Discord instance.
 
 ## Installation
 
-A pre-compiled binary can be downloaded from the [Releases](https://github.com/Marc3842h/rpc-wine/releases)
-tab. If you prefer building from source, the `build.sh` script can be used
-to build the library. You'll end up in both cases with a `discord-rpc.dll.so` file.
+Installation of this library can be done with a few simple steps.
 
-Install the library into a path that Wine can find. The
-`WINEDLLPATH` environment is used for setting dll override paths.
-Append to that environment variable your directory containing
-the `discord-rpc.dll.so` file.
+#### Dependencies
 
-Then configure a Wine DLL override in `winecfg`:
+Install the following dependencies, they are required for either building
+or running RPC Wine:
 
-![img](https://wontfix.club/i/WAXlzUuh.png)
+* make
+* wine-devel (including `winegcc` and headers)
+* RapidJson
 
-After that, start the game that has Discord RPC support
-and Wine will automatically redirect all Discord RPC calls
-to our `discord-rpc.dll.so`, which re-implements a few functions
-from the original Discord RPC library.
+#### Building
 
-## Limitations
+After installing dependencies and cloning the repository, the `build.sh`
+can be used to build both a 32-bit aswell as a 64-bit version of this library.
+The resulting binaries can be found in the `bin32` respectively the `bin64` directory
+in the source tree.
 
-This Wine DLL override only overrides [`discord-rpc.dll`](https://github.com/discordapp/discord-rpc). If your game
-uses a [different implementation](https://github.com/discordapp/discord-rpc#wrappers-and-implementations)
-this library will not work.
+#### Setup in Wineprefix
 
-Currently also only a limited subset of Discord RPC functions
-are implemented.
+Append to the `WINEDLLPATH` environment variable both the `bin32` and
+`bin64` directory containing the built library.
+
+Then configure your Wineprefix (`winecfg`) to add `discord-rpc`
+(without `.dll` extension) to the DLL overrides list:
+
+![winecfg](https://wontfix.club/i/UUusAV3s.png)
+
+and mark it to prefer the Builtin version over the native one:
+
+![winecfg edit override](https://wontfix.club/i/ihlrxiAp.png)
+
+## Restrictions
+
+RPC Wine currently has not all Discord-RPC methods implemented. This will
+be solved with future updates. However, there are other problems with RPC Wine:
+
+RPC Wine is **not** able to run by design:
+
+* Games which don't use the native `discord-rpc.dll` library but their own or a third-party wrapper.
+* Games with statically linked `discord-rpc.dll`
--- build.sh
+++ build.sh
@@ -1,107 +1,165 @@
 #!/usr/bin/env bash
 
+# Exit as soon as a command fails
+set -e
+
+# Discover directory where this script is located
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-strip=false
-_32bit=false
+# CLI options
+skip_32bit=false
+skip_64bit=false
 
-for i in "$@"; do
-    case ${i} in
-        -s|--strip) strip=true; shift ;;
-        -32|--32bit) _32bit=true; shift ;;
-        *) echo "Usage: $0 [-s|--strip] [-32|--32bit]"; exit 1 ;;
+# Parse them
+positional=()
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case "$key" in
+        -s32|--skip32|--skip32bit|--skip32-bit|--skip-32-bit) echo "Skipping 32-bit..."; skip_32bit=true; shift ;;
+        -s64|--skip64|--skip64bit|--skip64-bit|--skip-64-bit) echo "Skipping 64-bit..."; skip_64bit=true; shift ;;
     esac
-    shift
 done
 
-# Delete previous directory if it exists
-if [ -e "/tmp/discord-rpc" ]; then
-    rm -rf "/tmp/discord-rpc"
-fi
+# Restore positional parameters
+set -- "${positional[@]}"
 
-# Create build directory and copy files into it
-mkdir -p /tmp/discord-rpc
-cp -r "$script_dir/src/." "/tmp/discord-rpc/"
-cp "$script_dir/discord-rpc.spec" "/tmp/discord-rpc/discord_rpc.spec"
+# .cc/.hh -> .cpp/.hpp file extension converter
+fix_file_extension() {
+    for file in $(find $1 -name '*.cc' -or -name '*.hh'); do
+        filename=$(basename -- "$file")
+        extension="${filename##*.}"
+        filename="${filename%.*}"
 
-# Fix up file extension
-# Winemaker doesn't detect C++ source and header files with .hh and .cc extension
-for file in $(find /tmp/discord-rpc/ -name '*.cc' -or -name '*.hh'); do
-    filename=$(basename -- "$file")
-    extension="${filename##*.}"
-    filename="${filename%.*}"
+        if [ "$extension" == "cc" ]; then
+            mv "$file" "$(dirname ${file})/`basename $file .cc`.cpp"
 
-    if [ "$extension" == "cc" ]; then
-        mv "$file" "$(dirname ${file})/`basename $file .cc`.cpp"
+            # Fix includes
+            sed -i 's/\.cc/.cpp/g' "$(dirname ${file})/$filename.cpp"
+            sed -i 's/\.hh/.hpp/g' "$(dirname ${file})/$filename.cpp"
+        fi
 
-        # Fix includes
-        sed -i 's/\.cc/.cpp/g' "$(dirname ${file})/$filename.cpp"
-        sed -i 's/\.hh/.hpp/g' "$(dirname ${file})/$filename.cpp"
+        if [ "$extension" == "hh" ]; then
+            mv "$file" "$(dirname ${file})/`basename $file .hh`.hpp"
+
+            # Fix includes
+            sed -i 's/\.cc/.cpp/g' "$(dirname ${file})/$filename.hpp"
+            sed -i 's/\.hh/.hpp/g' "$(dirname ${file})/$filename.hpp"
+        fi
+    done
+}
+
+# Builds RPC Wine as 32-bit shared library
+build_32bit() {
+    # Delete previous directory if it exists
+    if [ -e "/tmp/discord-rpc/32bit/discord-rpc" ]; then
+        rm -rf "/tmp/discord-rpc/32bit/discord-rpc"
     fi
 
-    if [ "$extension" == "hh" ]; then
-        mv "$file" "$(dirname ${file})/`basename $file .hh`.hpp"
+    # Create build directory and copy files into it
+    mkdir -p "/tmp/discord-rpc/32bit/discord-rpc"
+    cp -r "$script_dir/src/." "/tmp/discord-rpc/32bit/discord-rpc/"
+    cp "$script_dir/discord-rpc.spec" "/tmp/discord-rpc/32bit/discord-rpc/discord_rpc.spec"
 
-        # Fix includes
-        sed -i 's/\.cc/.cpp/g' "$(dirname ${file})/$filename.hpp"
-        sed -i 's/\.hh/.hpp/g' "$(dirname ${file})/$filename.hpp"
-    fi
-done
+    # Fix up file extension
+    # Winemaker doesn't detect C++ source and header files with .hh and .cc extension
+    fix_file_extension "/tmp/discord-rpc/32bit/discord-rpc/"
 
-# Navigate into the build directory
-cd /tmp/discord-rpc
+    # Navigate into build directory
+    cd "/tmp/discord-rpc/32bit/discord-rpc/"
 
-# Create required build files
-if [ ${_32bit} == "true" ]; then
+    # Create Makefile and stuff with Winemaker
     winemaker --dll --wine32 --nobanner --nomfc --nomsvcrt --nosource-fix -lpthread .
 
     # Workaround bug that "-m32" won't get correctly added to Makefile
     # even thought we passed "--wine32" with winemaker.
-    cextra_line=$(grep -n -m 1 "CEXTRA" "/tmp/discord-rpc/Makefile" | sed 's/\([0-9]*\).*/\1/')
-    cxxextra_line=$(grep -n -m 1 "CXXEXTRA" "/tmp/discord-rpc/Makefile" | sed 's/\([0-9]*\).*/\1/')
-    ldflags_line=$(grep -n -m 1 "discord_rpc_dll_LDFLAGS" "/tmp/discord-rpc/Makefile" | sed 's/\([0-9]*\).*/\1/')
-    ldflags_line=$((ldflags_line + 1))
+    cextra_line=$(grep -n -m 1 "CEXTRA" "/tmp/discord-rpc/32bit/discord-rpc/Makefile" | sed 's/\([0-9]*\).*/\1/')s
+    cxxextra_line=$(grep -n -m 1 "CXXEXTRA" "/tmp/discord-rpc/32bit/discord-rpc/Makefile" | sed 's/\([0-9]*\).*/\1/')s
+    ldflags_line=$(grep -n -m 1 "LDFLAGS" "/tmp/discord-rpc/32bit/discord-rpc/Makefile" | sed 's/\([0-9]*\).*/\1/')
+    ldflags_line=$((ldflags_line + 1))s
 
-    cextra_line="$cextra_line"s
-    cxxextra_line="$cxxextra_line"s
-    ldflags_line="$ldflags_line"s
+    sed -i "$cextra_line/$/ -m32/" "/tmp/discord-rpc/32bit/discord-rpc/Makefile"
+    sed -i "$cxxextra_line/$/ -m32/" "/tmp/discord-rpc/32bit/discord-rpc/Makefile"
+    sed -i "$ldflags_line/$/ -m32/" "/tmp/discord-rpc/32bit/discord-rpc/Makefile"
 
-    sed -i "$cextra_line/$/ -m32/" "/tmp/discord-rpc/Makefile"
-    sed -i "$cxxextra_line/$/ -m32/" "/tmp/discord-rpc/Makefile"
-    sed -i "$ldflags_line/$/ -m32/" "/tmp/discord-rpc/Makefile"
-else
+    # The command below will fail so don't exit out if that happens
+    set +e
+
+    # Workaround for Ubuntu bug that Wine would use 64-bit libraries for linking against 32-bit targets
+    winegcc_output=$(winegcc -v -m32 /dev/zero 2>&1)
+
+    # Restore exiting out as soon as a command fails
+    set -e
+
+    # Check if WineGCC has 64-bit default libraries while building RPC Wine in 32-bit
+    if [[ "$winegcc_output" == *"lib64/wine/libwinecrt0.a"* ]]; then
+        echo "Detected Wine failure, trying to fix it up..."
+
+        fixed_string="-nodefaultlibs /usr/lib/i386-linux-gnu/wine/libwinecrt0.a -luser32 -lkernel32 -lgdi32"
+
+        sed -i "${cextra_line::-1} $fixed_string" "/tmp/discord-rpc/32bit/discord-rpc/Makefile"
+        sed -i "${cxxextra_line::-1} $fixed_string" "/tmp/discord-rpc/32bit/discord-rpc/Makefile"
+    fi
+
+    # Build the project
+    make
+
+    # Delete previous result directory if it exists
+    if [ -e "$script_dir/bin32" ]; then
+        rm -rf "$script_dir/bin32"
+    fi
+
+    # Create build result directory
+    mkdir -p "$script_dir/bin32"
+
+    # Copy result file to script directory
+    cp "/tmp/discord-rpc/32bit/discord-rpc/discord_rpc.dll.so" "$script_dir/bin32/discord-rpc.dll.so"
+}
+
+# Builds RPC Wine as 64-bit shared library
+build_64bit() {
+    # Delete previous directory if it exists
+    if [ -e "/tmp/discord-rpc/64bit/discord-rpc" ]; then
+        rm -rf "/tmp/discord-rpc/64bit/discord-rpc"
+    fi
+
+    # Create build directory and copy files into it
+    mkdir -p "/tmp/discord-rpc/64bit/discord-rpc"
+    cp -r "$script_dir/src/." "/tmp/discord-rpc/64bit/discord-rpc/"
+    cp "$script_dir/discord-rpc.spec" "/tmp/discord-rpc/64bit/discord-rpc/discord_rpc.spec"
+
+    # Fix up file extension
+    # Winemaker doesn't detect C++ source and header files with .hh and .cc extension
+    fix_file_extension "/tmp/discord-rpc/64bit/discord-rpc/"
+
+    # Navigate into build directory
+    cd "/tmp/discord-rpc/64bit/discord-rpc/"
+
+    # Create Makefile and stuff with Winemaker
     winemaker --dll --nobanner --nomfc --nomsvcrt --nosource-fix -lpthread .
+
+    # Build the project
+    make
+
+    # Delete previous result directory if it exists
+    if [ -e "$script_dir/bin64" ]; then
+        rm -rf "$script_dir/bin64"
+    fi
+
+    # Create build result directory
+    mkdir -p "$script_dir/bin64"
+
+    # Copy result file to script directory
+    cp "/tmp/discord-rpc/64bit/discord-rpc/discord_rpc.dll.so" "$script_dir/bin64/discord-rpc.dll.so"
+}
+
+# Build 32-bit library if we don't skip
+if [[ "$skip_32bit" != "true" ]]; then
+    build_32bit
 fi
 
-# Exit out of the build process if it didn't work
-if [ $? -ne 0 ]; then
-    echo -e "\e[91mFailed to create required files to build the project. Is Winemaker available?\e[0m"
-    exit $?
+# Build 64-bit library if this is a 64-bit machine
+if [[ "$skip_64bit" != "true" ]] || [[ "`uname -a`" == "x86_64" ]]; then
+    build_64bit
 fi
-
-# Build the project
-make
-
-# Exit out of the build process if it didn't work
-if [ $? -ne 0 ]; then
-    echo -e "\e[91mFailed to build the project. Did you install required dependencies?\e[0m"
-    exit $?
-fi
-
-# Go back to top level directory
-cd "$script_dir"
-
-# Delete previous result binary if it exists
-if [ -e "$script_dir/discord-rpc.dll.so" ]; then
-    rm -rf "$script_dir/discord-rpc.dll.so"
-fi
-
-# Copy result file to script directory
-cp "/tmp/discord-rpc/discord_rpc.dll.so" "$script_dir/discord-rpc.dll.so"
-
-if [ ${strip} == "true" ]; then
-    strip -s "$script_dir/discord-rpc.dll.so"
-fi
-
-# Success!
-echo -e "\e[32mSuccessfully built RPC Wine. Put the discord-rpc.dll.so into your WINEDLLPATH environment variable.\e[0m"
